34ffb27498a518d4ec80a16a4857e6e67de5a80d
fgiannotti <franco.giannotti@mercadolibre.com>
2021-02-06 20:47:50 -0300

    migrate to material UI + use react components instead of functional components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import './App.css';
-import React, { useState } from 'react'
-import {Container, Jumbotron} from 'reactstrap';
-import Form from './components/Form';
-import Logo from '*.svg'
+import React from 'react';
+import { Container, Card } from '@material-ui/core';
+import MyForm from './components/Form';
+import Logo from './logo.svg';
 
-const ALL_EXPENSES = [{id:1, name: 'Buy a book', amount:20},{id:2, name: 'Library', amount:200}]
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { expenses: [{ id: 1, name: 'Buy a book', amount: 20 }, { id: 2, name: 'Library', amount: 200 }] };
+    this.totalExpenses = this.totalExpenses.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
 
-function App() {
-  const [expenses, setExpense] = useState(ALL_EXPENSES)
-  return (
-    <Container className="text-center">
-      <Jumbotron fluid>
-      <h3 className='display-6 text-center'>
-          Expense Tracker React App
-          <img src={Logo} style={{ width: 50, height: 50 }} alt='react-logo' />
-        </h3>
-        <div className='text-center'>
-          <p>
-            Total Expense:{' '}
-            <span className='text-success'>
-              ${' '}
-              {expenses.reduce((accumulator, currentValue) => {
-                return (accumulator += parseInt(currentValue.amount))
-              }, 0)}
-            </span>
-          </p>
-        </div>
-        {}
-        <Form />
-      </Jumbotron>
-    </Container>
-   
-  );
+  onSubmitHandler(event) {
+    event.preventDefault();
+    const { expenses } = this.state;
+    console.log(expenses);
+    const name = event.target.expenseName.value;
+    const amount = event.target.expenseAmount.value;
+
+    const id = expenses[expenses.length - 1].id + 1;
+    const newExpense = { id, name, amount };
+
+    this.setState({ expenses: [...expenses, newExpense] });
+  }
+
+  totalExpenses() {
+    const { expenses } = this.state;
+    // eslint-disable-next-line max-len
+    return expenses.reduce((accumulator, currentValue) => (accumulator + parseInt(currentValue.amount, 10)), 0);
+  }
+
+  render() {
+    return (
+      // eslint-disable-next-line react/jsx-filename-extension
+      <Container className="text-center">
+        <Card fluid>
+          <h3 className="display-6 text-center">
+            Expense Tracker React App
+            <img src={Logo} alt="react-logo" />
+          </h3>
+          <div className="text-center">
+            <p>
+              Total Expense:
+              <span className="text-success">
+                $
+                {this.totalExpenses()}
+              </span>
+            </p>
+          </div>
+          <MyForm onSubmitHandler={this.onSubmitHandler} />
+        </Card>
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,50 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { BTForm,Col, FormGroup, Label,Input,Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+import {
+  FormGroup, Input, Button, FormLabel, TableRow,
+} from '@material-ui/core';
 
-const Form = () => (
-    <BTForm style={{margin:10}}>
-        <FormGroup className="row">
+const MyForm = ({ onSubmitHandler }) => (
+  <form onSubmit={onSubmitHandler} style={{ margin: 10 }}>
+    <FormGroup className="row">
 
-            <Label for="exampleEmail" sm={2}>
-                Name of Expense
-            </Label>
+      <FormLabel htmlFor="expenseName" sm={2}>
+        Name of Expense
+      </FormLabel>
 
-            <Col sm={4}>
-                <Input 
-                    type="text"
-                    name="name"
-                    id="expenseName"
-                    placeHolder="Name of expense?">
-                </Input>
-            </Col>
-        </FormGroup>
-        <FormGroup className="row">
-            <Label for="exampleEmail" sm={2}>
-                $Amount
-            </Label>
-            <Col sm={4}>
-                <Input 
-                    type="number"
-                    name="amount"
-                    id="expenseAmount"
-                    placeHolder="0.00"/>
-                    
-            </Col>
-        </FormGroup>
-        <Button type="submit" color="primary">
-            Add
-        </Button>
-    </BTForm>
-)
+      <TableRow sm={4}>
+        <Input
+          type="text"
+          name="expenseName"
+          id="expenseName"
+          placeholder="Name of expense?"
+        />
+      </TableRow>
+    </FormGroup>
+    <FormGroup className="row">
+      <FormLabel htmlFor="expenseAmount" sm={2}>
+        Expense Amount
+      </FormLabel>
 
-export default Form
+      <TableRow sm={4}>
+        <Input
+          type="number"
+          name="expenseAmount"
+          id="expenseAmount"
+          placeholder="0.00"
+        />
+
+      </TableRow>
+
+    </FormGroup>
+    <Button type="submit" color="primary">
+      Add
+    </Button>
+  </form>
+);
+
+MyForm.propTypes = {
+  onSubmitHandler: PropTypes.func.isRequired,
+};
+export default MyForm;
